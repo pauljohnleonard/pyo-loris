24812f79416724a17bbc87137b11afbf233dea62
PJL <pauljohnleonard@gmail.com>
2015-02-11 13:45:23 +0000

    WIP integrating new synth to gui2

--- src/synth.py
+++ src/synth.py
@@ -7,8 +7,6 @@ class Synth:
     """
     LOris synthesizer that can be controlled in real time.
 
-    synt.setTarget(feature,time)
-
     """
 
     def __init__(self, freqs, ampTone, ampNoise):
@@ -17,58 +15,53 @@ class Synth:
 
 
         # Creat an array of oscillators (one per partial)
-        self.parts = PartialOsc(freq=freqs, ampTone=ampTone, ampNoise=ampNoise)
 
-        osc = self.parts.osc
+        # self.parts = PartialOsc(freq=freqs, ampTone=ampTone, ampNoise=ampNoise)
 
-        for key in range(len(osc)):
-            self.mixer.addInput(key, osc[key])
+        init_phase = 0
+
+        self.white = Noise()
+
+        # low pass filter the noise
+        # loris uses 4 forward and back coeffecients but pyo only has a bi quad.
+        # TODO implement general digital filter in pyo
+
+        self.mod = Biquad(self.white, freq=500, q=1, type=0, mul=ampNoise, add=ampTone)
+
+        self.osc = Sine(freq=freqs, mul=self.mod, phase=init_phase)
+
+
+
+        for key in range(len(self.osc)):
+            self.mixer.addInput(key, self.osc[key])
             self.mixer.setAmp(key, 0, 1.0)
             self.mixer.setAmp(key, 1, 1.0)
 
         self.mixer.out()
 
 
-class PartialOsc:
-    """
-    Oscillator for a single Partial
-    
-    part is a loris Partial list.
-    
-    output(t)=(ampTone+ampNoise*LPF(whiteNoise)))*sin(freq*2*pi*t+phase)
- 
-    """
-
-    def __init__(self, freq, ampTone, ampNoise):
-        init_phase = 0
-
-        self.white = Noise()
-
-        # low pass filter the noise 
-        # loris uses 4 forward and back coeffecients but pyo only has a bi quad.
-        # TODO implement general digital filter in pyo
-        self.mod = Biquad(self.white, freq=500, q=1, type=0, mul=ampNoise, add=ampTone)
-
-        self.osc = Sine(freq=freq, mul=self.mod, phase=init_phase)
-
-
-    def out(self):
-        self.osc.out()
 
 
 class Channel:
-    def __init__(self, parts,name,index):
+    def __init__(self, index,spc_file):
 
 
+        if not  os.path.isfile(spc_file):
+            print spc_file, " Does not exist"
+            sys.exit(0)
+
+
+        self.parts= loris.importSpc(spc_file)
         self.noises = []
         self.tones = []
         self.freqs = []
 
-        print name,"======================================================================="
+        print spc_file,"======================================================================="
 
 
-        for i,part in enumerate(parts):
+        for i,part in enumerate(self.parts):
             print i,part.label()
+
             ampNoiseList = [(0, 0)]
             ampToneList = [(0, 0)]
             freqList = [(0, 0)]
@@ -76,6 +69,7 @@ class Channel:
             sr = 44100
 
             cnt = 0
+
 
             for bp in part:
                 t_sec = bp.time()
@@ -155,66 +149,61 @@ class Channel:
             self.noises.append(ampNoise)
 
 
+
+def setmix(chanA,chanB,fact):
+
+    N_CHAN_A = len(chanA.parts)  # ,len(parts_flute))
+    N_CHAN_B = len(chanB.parts)  # ,len(parts_flute))
+    N_SYNTH = max(N_CHAN_A, N_CHAN_B)
+
+    freqs = [None for _ in range(N_SYNTH)]
+    tones = [None for _ in range(N_SYNTH)]
+    noises = [None for _ in range(N_SYNTH)]
+
+    fact_A=fact
+    fact_B=1.0-fact
+
+    for i in range(N_SYNTH):
+        if i < N_CHAN_A:
+
+            if i < N_CHAN_B:
+                freqs[i] = chanA.freqs[i]*fact_A+chanB.freqs[i]*fact_B
+                tones[i] = chanA.tones[i]*fact_A+chanB.tones[i]*fact_B
+                noises[i] = chanA.noises[i]*fact_A+chanB.noises[i]*fact_B
+            else:
+                freqs[i] = chanA.freqs[i]
+                tones[i] = chanA.tones[i]*fact_A
+                noises[i] = chanA.noises[i]*fact_A
+
+
+        else:
+            if i < N_CHAN_B:
+                freqs[i] = chanB.freqs[i]
+                tones[i] = chanB.tones[i]*fact_B
+                noises[i] = chanB.noises[i]*fact_B
+
+    return freqs,tones,noises
+
 if __name__ == "__main__":
 
 
     s = Server().boot()
-    index=Sig(0.0)
-    index.ctrl()
-
-    p=loris.Partial()
-    spc_clar = "samples/clarinetM.spc"
-    parts_clar = loris.importSpc(spc_clar)
-    chan_clar = Channel(parts_clar,"clarinet", index)
-
-    spc_flute = "samples//fluteM.spc"
-    parts_flute = loris.importSpc(spc_flute)
-    chan_flute = Channel(parts_flute,"Flute", index)
-
-    N_CHAN_C = len(parts_clar)  # ,len(parts_flute))
-    N_CHAN_F = len(parts_flute)  # ,len(parts_flute))
-    N_SYNTH = max(N_CHAN_C, N_CHAN_F)
+    a=Sig(0.0)
+    index=SigTo(a,time=2.0)
+    a.ctrl()
 
 
+    spc_clar="samples/clarinetM.spc"
+    chan_clar = Channel( index,spc_clar)
 
-    if True:
-        freqs = [None for _ in range(N_SYNTH)]
-        tones = [None for _ in range(N_SYNTH)]
-        noises = [None for _ in range(N_SYNTH)]
-
-
-        fact_c=Sig(1.0)
-        fact_f=1.0-fact_c
-        fact_c.ctrl()
+    spc_flute = "samples/fluteM.spc"
+    chan_flute = Channel( index,spc_flute)
 
 
+    fact=Sig(0.0)
 
-        for i in range(N_SYNTH):
-            if i < N_CHAN_C:
-
-                if i < N_CHAN_F:
-                    freqs[i] = chan_flute.freqs[i]*fact_f+chan_clar.freqs[i]*fact_c
-                    tones[i] = chan_flute.tones[i]*fact_f+chan_clar.tones[i]*fact_c
-                    noises[i] = chan_flute.noises[i]*fact_f+chan_clar.noises[i]*fact_c
-                else:
-                    freqs[i] = chan_clar.freqs[i]
-                    tones[i] = chan_clar.tones[i]*fact_c
-                    noises[i] = chan_clar.noises[i]*fact_c
-
-
-            else:
-                if i < N_CHAN_F:
-                    freqs[i] = chan_flute.freqs[i]
-                    tones[i] = chan_flute.tones[i]*fact_f
-                    noises[i] = chan_flute.noises[i]*fact_f
-
-
-    else:
-        freqs = chan_flute.freqs
-        tones = chan_flute.tones
-        noises = chan_flute.noises
-
-
+    fact.ctrl()
+    freqs,tones,noises=setmix(chan_clar,chan_flute,fact)
     synth = Synth(freqs, tones, noises)
 
     s.gui('locals()')
